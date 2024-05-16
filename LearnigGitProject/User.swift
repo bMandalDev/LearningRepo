277303e97cc03dc3de8175d1110b2632d40c97b9
Binod <canopus.binod@gmail.com>
2024-05-16 18:12:51 +0530

    added User class
    added user class properties

---
+++ LearnigGitProject/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  LearnigGitProject
+//
+//  Created by Binod Mandal on 16/05/24.
+//
+
+import Foundation
+
+struct User {
+    let name: String
+    let email: String
+    let imageUrl: String
+}
